525b15b65e23436de5e49cc7357f3c3748147001
jinyan yang <jyyoung@live.com>
2019-02-11 11:37:01 +1100

    Differences fro Hufkens: 1. lagged water and temperate calculation; 2. pet function; 3. not uing MCMC but Deoptim

--- models/hufkens/main.R
+++ models/hufkens/main.R
@@ -1,24 +1,27 @@
+library(DEoptim)
+library(Evapotranspiration)
+library(HIEv)
+library(data.table)
+library(DoBy)
+
+if(!dir.exists("download"))dir.create("download")
+source('models/hufkens/phenoGrass.R')
+source('models/hufkens/pen_mon.R')
+
+if(!file.exists('gcc.met.df.rds')){
+  source('models/hufkens/get_gcc_met_Euc.R')
+}
+
 # # to be fitted
 # # f.h,f.t.opt, f.extract,f.sec
 # # needed par values
 # # swc.wilt,swc.capacity,swc.initial
-
-# hufkens did not mention what field capacity and wilting points are and where from
-# they used an arbitory tmax of 45
+# hufkens used pars values in Tabl S5
 swc.wilt <- 100
 swc.capacity <- 400
 t.max <- 45
 
-# get GCC and normalis
 # # met data is needed 16 days before gcc 
-# gcc.df <- data.frame(Date=NA,
-#                      GCC=0.3 + runif(32,-0.1,.1),
-#                      map=800,
-#                      tmean = rep(35,32),
-#                      rad = 1800 + runif(32,-100,100),
-#                      ppt = floor(runif(32,0,1.1))*20)
-# gcc.df$GCC.norm <- (gcc.df$GCC - min(gcc.df$GCC)) / 
-#   (min(gcc.df$GCC) + max(gcc.df$GCC))
 
 # target function to minimise
 target.func <- function(dat,
@@ -45,15 +48,15 @@ target.func <- function(dat,
   return(abs(cvmae))
 }
 
-# target.func(gcc.met.df,c(200,30,0.8,0.5))
+
 # # get the fitting running with deoptim
+# order of pars
 # f.h=200
 # f.t.opt=30
 # f.extract <- 0.5
 # f.sec <- 0.05
 # f.growth <- 0.002
-
-lower <- c(100, 10, 0.4, 0.01, 0.001) # changed minimum Kmax from 2 to 5
+lower <- c(100, 10, 0.4, 0.01, 0.001) 
 upper <- c(300, 40, 0.9, 0.09, 0.005)
 NPmax <- 100
 maxiter <- 60
@@ -66,7 +69,9 @@ set.seed(1234)
 #- model with no change in photosynthetic capacity
 library(DEoptim)
 library(Evapotranspiration)
-data("constants")
+data("constants") #this is used for penman et value; data from Adleide
+
+# fit eucface with Deoptim, It seems this is not too different from MCMC
 hufkens.fit <- DEoptim(fn=target.func,lower=lower,upper=upper,
                        dat=gcc.met.df,
                        DEoptim.control(NP = NPmax,itermax=maxiter,
@@ -76,19 +81,18 @@ hufkens.fit <- DEoptim(fn=target.func,lower=lower,upper=upper,
 
 hufkens.fit.best <- unname(hufkens.fit$optim$bestmem)
 
-
+# make predictions
 gcc.met.df$c.pred<- phenoGrass.func(gcc.met.df,264.807,30.046,0.707,0.05,0.002698,
                                     swc.wilt = swc.wilt,
                                     swc.capacity = swc.capacity,
                                     t.max = t.max)
 
+# plots
+# 1:1
 plot(c.pred~GCC.norm,data = gcc.met.df)
 abline(a=0,b=1)
 
-
+# time 
 plot(GCC.norm~Date,data = gcc.met.df)
-# abline(a=0,b=1)
-
 points(c.pred~Date,data = gcc.met.df,pch=16,col='coral')
-
 legend('topleft',legend = c('Obs','MOD'),pch=16,col=c('black','coral'))
--- models/hufkens/phenoGrass.R
+++ models/hufkens/phenoGrass.R
@@ -1,18 +1,19 @@
-# get a scaling factor
+# get a scaling factor####
 scaling.f.func <- function(map,f.h){
   # map is input, h should be fitted
   map / (map + f.h)
 }
-# t response function
+# t response function####
 t.func <- function(t.mean,f.t.opt,t.max){
   return((t.max-t.mean)/(t.max-f.t.opt)*(t.mean/f.t.opt)^(f.t.opt/(t.max-f.t.opt)))
 }
+# code to test t.func
 # tmean.vec<- seq(0,40)
 # g.vec <- t.func(tmean.vec,33,45)
 # plot(g.vec~tmean.vec)
 
-# build the pheno grass model as in Hufkens 2016
-# the Penman equation is a different version though
+# build the pheno grass model as in Hufkens 2016#####
+# the Penman equation is a different version 
 # here used the equation modified from the evapotranspiration R package
 phenoGrass.func <- function(gcc.df,
                             f.h,
@@ -23,6 +24,8 @@ phenoGrass.func <- function(gcc.df,
                             swc.wilt ,
                             swc.capacity ,
                             t.max){
+  
+  # set the lag factor; in num of days
   day.lay <- 16
   sf.value <- scaling.f.func(mean(gcc.df$map),f.h)
   gcc.df$cover <- sf.value * gcc.df$GCC.norm 
@@ -30,13 +33,12 @@ phenoGrass.func <- function(gcc.df,
   swc.vec <- c()
   swc.vec[day.lay] <- 200
   et <- c()
-  # swc.vec[1] <- swc.inital
   cover.pred.vec <- c()
   cover.pred.vec[day.lay] <- gcc.df$cover[!is.na(gcc.df$cover)][1]
   water.avi <- c()
   # water.avi[1] <- swc.inital - swc.wilt
   water.lag <- c()
-  water.lag[day.lay -1] <- water.avi.inital <- 50 #need to be calculated in the future
+  water.lag[day.lay - 1] <- water.avi.inital <- 50 #need to be calculated in the future
   t.m <- c()
 
   # calcualte the par values
@@ -45,19 +47,21 @@ phenoGrass.func <- function(gcc.df,
   rad.max <-  max(gcc.df$PPFD,na.rm=TRUE)
   gcc.df$rad.norm <- (gcc.df$PPFD - rad.min) / (rad.max - rad.min)
   
+  # model start
   for (nm.day in day.lay:(nrow(gcc.df)-1)){
     # plant avialbe water
     water.avi[nm.day] <- max(0,swc.vec[nm.day]- swc.wilt)
     
+    # define the legency effect 
     i=0
     while(i<day.lay & (nm.day-i)>0){
       i=i+1
     }
     
-    water.lag[nm.day] <- min(water.avi[(nm.day-i):nm.day],na.rm=TRUE)
-    t.m[nm.day] <- mean(gcc.df$Tair[(nm.day-i):nm.day],na.rm=TRUE)
-    # evaportanspiration from Hargreaves 1985
-    # Date,PPFD,Tair,Tmax,Tmin,RHmax,RHmin,u2
+    water.lag[nm.day] <- min(water.avi[(nm.day-i):nm.day],na.rm=TRUE) #note this is different from the Hufkens
+    t.m[nm.day] <- mean(gcc.df$Tair[(nm.day-i):nm.day],na.rm=TRUE) #hufkens used 15 days
+    # hufkens used evaportanspiration from Hargreaves 1985
+    # here is from evapotranspiration R package
     et[nm.day] <- pet.func(gcc.df$Date[nm.day],gcc.df$PPFD[nm.day],
                            gcc.df$Tair[nm.day],gcc.df$Tmax[nm.day], gcc.df$Tmin[nm.day],
                            gcc.df$RHmax[nm.day],gcc.df$RHmin[nm.day], gcc.df$u2[nm.day])
@@ -77,16 +81,17 @@ phenoGrass.func <- function(gcc.df,
       d=1
     }
     
+    # give min
     cover.pred.vec[nm.day] <- max(0,min(cover.pred.vec[nm.day],
                                         cover.max))
+    # get g value; t depedence
     g.value <- t.func(t.m[nm.day],f.t.opt,t.max)
- 
+    # calculate plant cover
     cover.pred.vec[nm.day+1] <- cover.pred.vec[nm.day] + 
       g.value * sf.value * f.growth * water.lag[nm.day] *
       gcc.df$rad.norm[nm.day] * 
       (1 - cover.pred.vec[nm.day] / cover.max)  -
       d * f.sec * (1 - cover.pred.vec[nm.day]) * cover.pred.vec[nm.day]
-    
     
     # calculate swc
     swc.vec[nm.day] <- max(0,min(swc.capacity,swc.vec[nm.day]))
@@ -103,12 +108,6 @@ phenoGrass.func <- function(gcc.df,
   
   return(cover.pred.vec)
 }
-
-# # gcc.df,
-# # f.h,f.t.opt,f.extract,f.sec,
-# # swc.wilt ,
-# # swc.capacity ,
-# # t.max
 
 # phenoGrass.func(gcc.df,200,25,0.5,0.05,.001,100,400,45)
 
