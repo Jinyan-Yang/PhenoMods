380cbde64b17e60c89b77ce4d29a43cc487d5abb
JinyanYang <jyyoung@live.com>
2020-04-22 16:36:03 +1000

    Model documentation update: 1 Emax implemetation; 2 The low sensitivity to Psi.leaf.min

---
+++ r/check_hydro.R
@@ -0,0 +1,36 @@
+source('r/function_hydro.R')
+# creat swc vector
+swc.vec <- seq(0.05,0.13,by = 0.001)
+# get psi
+psi.vec <- psi.s.func(swc.vec,
+                      psi.e = -0.03,#KPa
+                      b = 4.26, 
+                      swc.sat = 0.13)
+plot(psi.vec~swc.vec)
+
+# get fraction of max E
+fract.vec <- sapply(swc.vec,e.frac.func,
+                    lai=4,
+                    k.plant = 0.08,
+                    psi.e = -0.03e-3,#MPa
+                    b = 15, 
+                    swc.sat = 0.13,
+                    psi.min = -2,
+                    k.sat = 10)
+plot(fract.vec~swc.vec)
+# change min plant psi
+fract.vec.dry <- sapply(swc.vec,e.frac.func,
+                        lai=4,
+                        k.plant = 0.08,
+                        psi.e = -0.03e-3,#KPa
+                        b = 15, 
+                        swc.sat = 0.13,
+                        psi.min = -8,
+                        k.sat = 10)
+
+# plot
+plot(fract.vec~swc.vec,type='l',lwd=3,col='grey',
+     xlab=expression(theta[soil]),
+     ylab=expression(f[water.avi]))
+points(fract.vec.dry~swc.vec,type='l',lwd=3,col='red')
+
