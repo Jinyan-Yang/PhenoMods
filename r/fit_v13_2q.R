85359182a50d84d506cab36009e070f2e40c2ba9
JinyanYang <jyyoung@live.com>
2021-03-30 15:39:03 +1100

    updated plot function

--- r/fit_v13_2q.R
+++ r/fit_v13_2q.R
@@ -1,4 +1,4 @@
-day.lag <- 1
+day.lag <- 3
 source('r/pace_data_process.R')
 source('r/ym_data_process.R')
 source('r/process_cw_gcc.R')
@@ -8,20 +8,19 @@ devtools::source_url("https://github.com/Jinyan-Yang/colors/blob/master/R/col.R?
 library(zoo)
 library(foreach)
 library(doParallel)
-# library(mvtnorm)
-
+#
 # fit pace####
 species.vec <- c('Luc','Fes','Rye',
                  'Dig', 'DigBis', 'Kan', 
                  'KanWal','Pha', 'PhaSub', 
                  'Rho',  'Wal')
 # seq_along(species.vec)
-for(i in c(2,3,4,6,10)){
+for(i in c(1,2,3,4,6,10)){
   fit.mcmc.2q.func(df = gcc.met.pace.df,
                    n.iter = 20000,
                      species.in=species.vec[i],prep.in = 'Control', temp.in ='Ambient',
                      my.fun = phenoGrass.func.v13,
-                   out.nm.note='v13.2q', use.smooth = TRUE)
+                   out.nm.note='v13.2q', use.smooth = TRUE,cal.initial = T,day.lag = 3)
   
 }
 
@@ -30,7 +29,7 @@ ym.18.df <- get.ym.func(18)
 fit.mcmc.2q.func(df=ym.18.df,n.iter = 20000,
                    species.in='ym',prep.in = 'Control', temp.in ='Ambient',
                    my.fun = phenoGrass.func.v13,out.nm.note='v13.2q',use.smooth = TRUE,
-                   swc.capacity = 0.3,swc.wilt = 0.05,day.lag=1,bucket.size = 1000)
+                   swc.capacity = 0.3,swc.wilt = 0.05,bucket.size = 1000,cal.initial = F,day.lag = 3)
 # 
 # fit.mcmc.2q.func(df=ym.18.df,n.iter = 1000,
 #                  species.in='ym',prep.in = 'Control', temp.in ='Ambient',
@@ -51,16 +50,15 @@ for (i in seq_along(site.vec)){
 
 source('r/proces_modis_sites.R')
 # seq_along(modis.sites.vec)
-for (i in c(1,4,6,11,15)){
+for (i in c(14)){
   
   limits.vec <- limit.ls[[i]]
   fit.mcmc.2q.func(df=gcc.met.modis.df,
                    species.in=modis.sites.vec[i],prep.in = 'Control', temp.in ='Ambient',
                    my.fun = phenoGrass.func.v13,out.nm.note='v13.2q.rooting.oldproposal.newNDVI.',use.smooth = FALSE,
-                   swc.capacity = 0.3,swc.wilt = 0.05,n.iter = 20000,bucket.size = 1000,
-                   norm.min.max = limits.vec)
+                   swc.capacity = 0.45,swc.wilt = 0.05,n.iter = 20000,bucket.size = 1000,
+                   norm.min.max = limits.vec,cal.initial = FALSE,day.lag = 3)
 }
-
 
 # make plots####
 source('r/plot.mcmc.r')
@@ -68,7 +66,7 @@ source('r/plot.mcmc.r')
 pdf('figures/v13.2q.pdf',width = 8,height = 8*0.618)
 
 # ym
-plot.mcmc.func.2q(ym.18.df,'ym','Control','Ambient',
+plot.mcmc.func.2q(ym.18.df,species.in='ym',prep.in = 'Control','Ambient',
                   my.fun = phenoGrass.func.v13,
                   nm.note='v13.2q',use.smooth = TRUE,swc.in.cap = 0.3,swc.in.wilt = 0.05,bucket.size = 1000)
 plot.title.func('YM') 
@@ -96,39 +94,36 @@ dev.off()
 
 # # plot modis
 
-pdf('figures/v13.2q.modis.tussock600.pdf',width = 8,height = 8*0.618)
+pdf('figures/v13.2q.modis.pdf',width = 8,height = 8*0.618)
 
 # modis sites
-for (i in c(13)) {
+for (i in c(12)) {
   limits.vec <- limit.ls[[i]]
-  plot.mcmc.func.2q.modis(df=gcc.met.modis.df,
-                    species.in=modis.sites.vec[i],
-                    prep.in='Control',
-                    temp.in='Ambient',
-                    my.fun = phenoGrass.func.v13,
-                    nm.note='v13.2q.rooting.oldproposal.newNDVI.',use.smooth = FALSE,norm.min.max = limits.vec,
-                    swc.in.cap = 0.3,swc.in.wilt = 0.05,bucket.size = 1000)
-  
+  plot.mcmc.func.2q.modis(df=gcc.met.modis.df,#[year(gcc.met.modis.df$Date)==2014,]
+                          species.in=modis.sites.vec[i],
+                          prep.in='Control',
+                          temp.in='Ambient',
+                          my.fun = phenoGrass.func.v13,
+                          nm.note='v13.2q.rooting.oldproposal.newNDVI.',use.smooth = FALSE,norm.min.max = limits.vec,
+                          swc.in.cap = 0.45,swc.in.wilt = 0,bucket.size = 1000)
+  # 
   plot.title.func(modis.sites.vec[i])
   
+  # modis
+  fn <- sprintf('cache/v13.2q.rooting.oldproposal.newNDVI.chain.%s.Control.Ambient.rds',modis.sites.vec[i])
+  chain.3.ls = readRDS(fn)
+  lapply(chain.3.ls, plot.check.mcmc.func,species.in=modis.sites.vec[i])
+  
   # 
-#   combine_pdfs = function(path, output_pdf) {
-#     system(sprintf("pdftk %s/*pdf cat output %s"), path, output_pdf)
-#   }
-}
-
-# modis
-fn <- c('cache/v13.2q.rooting.oldproposal.newNDVI.chain.tussock_600.Control.Ambient.rds')
-chain.3.ls = readRDS(fn)
-lapply(chain.3.ls, plot.check.mcmc.func,species.in='tussock<600')
-
-# 
-par(mfrow=c(3,2),mar=c(5,5,1,1))
-for(i in 1:6){
-  
-  plot.line.mcmc.func(chain.3.ls,i,range.iter = 13000:15000)
+  par(mfrow=c(3,2),mar=c(5,5,1,1))
+  for(par.num in 1:6){
+    
+    plot.line.mcmc.func(chain.3.ls,par.num,range.iter =  round(3*nrow(chain.3.ls[[1]])/4):nrow(chain.3.ls[[1]]))
+    
+  }
   
 }
+
 
 dev.off()
 
@@ -147,7 +142,17 @@ for(i in  c(1,2,3,4,6,10)){
   fn <- sprintf('cache/smv13.2qchain.%s.Control.Ambient.rds',species.vec[i])
   chain.3.ls = readRDS(fn)
   lapply(chain.3.ls, plot.check.mcmc.func,species.in=species.vec[i])
+  
+  par(mfrow=c(3,2),mar=c(5,5,1,1))
+  for(par.num in 1:6){
+    
+    plot.line.mcmc.func(chain.3.ls,par.num,range.iter =  round(1:nrow(chain.3.ls[[1]])))
+    
+  }
 }
+
+# 
+
 
 # ym
 fn <- sprintf('cache/smv13.2qchain.%s.Control.Ambient.rds','ym')
@@ -180,7 +185,7 @@ lapply(chain.3.ls, plot.check.mcmc.func,species.in='Pasture<1000')
 par(mfrow=c(3,2),mar=c(5,5,1,1))
 for(i in 1:6){
   
-  plot.line.mcmc.func(chain.3.ls,i,range.iter = 13000:15000)
+  plot.line.mcmc.func(chain.3.ls,i,range.iter = 10000:20000)
   
 }
 # pdf('figures/v13.ym.2q.diag.pdf',width = 8,height = 8*0.618)
